e7d0974564c3586e17ee04f54bf97271c65a2f64
RamonVH18 <ramon.valencia253272@potros.itson.edu.mx>
2025-02-18 19:21:24 -0700

    Creacion de Entidades y Usuario

--- capaPersistenciaProyecto1/src/main/java/Entidades/Usuario.java
+++ capaPersistenciaProyecto1/src/main/java/Entidades/Usuario.java
@@ -5,9 +5,67 @@
 package Entidades;
 
 /**
- *
- * @author PC
+ * Esta clase representa a un usuario del sistema
+ * @author Ramon
  */
 public class Usuario {
+    private int idUsuario;
+    private String nombreUsuario;
+    private String contrasenha;
+    /**
+     * 
+     */
+    public Usuario() {
+    }
+    /**
+     * 
+     * @param idUsuario
+     * @param nombreUsuario
+     * @param contrasenha 
+     */
+    public Usuario(int idUsuario, String nombreUsuario, String contrasenha) {
+        this.idUsuario = idUsuario;
+        this.nombreUsuario = nombreUsuario;
+        this.contrasenha = contrasenha;
+    }
+    /**
+     * 
+     * @param nombreUsuario
+     * @param contrasenha 
+     */
+    public Usuario(String nombreUsuario, String contrasenha) {
+        this.nombreUsuario = nombreUsuario;
+        this.contrasenha = contrasenha;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContrasenha() {
+        return contrasenha;
+    }
+
+    public void setContrasenha(String contrasenha) {
+        this.contrasenha = contrasenha;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "idUsuario=" + idUsuario + ", nombreUsuario=" + nombreUsuario + ", contrasenha=" + contrasenha + '}';
+    }
+    
     
 }
